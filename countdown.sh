48b55b66aa3f50aac70cd7dfd6f980858de42acb
Chetan Tekam <tekamchetan3500@gmail.com>
2025-06-24 17:02:05 +0000

    Save progress: 680.1

--- countdown.sh
+++ countdown.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Program that counts down to zero from a given argument
 
 if [[ $1 -gt 0 ]]
@@ -6,6 +7,7 @@ then
   for (( i = $1; i > 0; i-- ))
   do
     echo $i
+    sleep 1
   done
 else
   echo Include a positive integer as the first argument.
